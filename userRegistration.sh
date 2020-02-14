43121f829eb5ce13a9149e28f4e10f25eab77cef
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-02-14 11:09:38 +0530

    ADDED : Function To Check Valid First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,18 @@
 #!/bin/bash -x
 echo "Welcome To User Registration"
+#Constants
+VALID_FIRST_NAME_PATTERN="^[A-Z]{1}[a-zA-Z]{2,}$"
+
+function validationCheck() {
+	userInput=$1
+	validationPattern=$2
+	if [[ $userInput =~ $validationPattern ]]
+	then
+		echo $name
+	else
+		echo "Invalid Input \" $name \""
+	fi
+}
+
+read -p "Enter First Name : " name
+validationCheck $name $VALID_NAME_PATTERN
