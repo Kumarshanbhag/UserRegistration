3d0b4e4e9cde5e837715924c874165fb0bbc0b6f
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-02-14 19:26:57 +0530

    ADDED : Function checks Valid Email ID

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,7 +1,9 @@
-#!/bin/bash  -x
+#!/bin/bash
+shopt -s extglob
 echo "Welcome To User Registration"
 #Constants
 VALID_NAME_PATTERN="^[A-Z]{1}[a-zA-Z]{2,}$"
+VALID_EMAIL_PATTERN="^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"
 
 function validationCheck() {
 	userInput=$1
@@ -10,7 +12,7 @@ function validationCheck() {
 	then
 		echo $userInput
 	else
-		echo "Invalid Input \" $userInput \""
+		echo "Invalid Input \"$userInput\" "
 		exit
 	fi
 }
@@ -19,3 +21,5 @@ read -p "Enter First Name : " firstName
 validationCheck $firstName $VALID_NAME_PATTERN
 read -p "Enter Last Name : " lastName
 validationCheck $lastName $VALID_NAME_PATTERN
+read -p "Enter Email : " email
+validationCheck $email $VALID_EMAIL_PATTERN
